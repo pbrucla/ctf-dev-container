686e88795b69b4bcff41113093609c0dd8436422
Benson Liu <bensonhliu@gmail.com>
2023-09-21 21:37:29 -0700

    fix: cargo installation

--- src/install-general.sh
+++ src/install-general.sh
@@ -7,7 +7,8 @@ apt install -y python3-pip ruby php
 export CARGO_INSTALL_ROOT='/usr/local'
 curl https://sh.rustup.rs -sSf | bash -s -- -y
 echo 'source $HOME/.cargo/env' >> $HOME/.bashrc
+source "$HOME/.cargo/env"
 
 # Install Python libs
 pip3 install --upgrade pip
-pip3 install --upgrade pwntools angr z3-solver requests aiohttp tqdm pycrypto
+pip3 install --upgrade pwntools angr z3-solver requests aiohttp tqdm pycrypto patchelf ROPgadget
