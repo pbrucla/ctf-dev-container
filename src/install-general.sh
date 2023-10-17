3f5bb97d94bcbab133a7ea67325406a2cbef20e0
Alexander Zhang <alex@alexyzhang.dev>
2023-10-16 17:45:19 -0700

    smh

--- src/install-general.sh
+++ src/install-general.sh
@@ -6,7 +6,7 @@ apt install -y python3-pip ruby php
 # Install Rust
 export CARGO_INSTALL_ROOT='/usr/local'
 curl https://sh.rustup.rs -sSf | bash -s -- -y
-echo 'source $HOME/.cargo/env' >> $HOME/.bashrc
+echo 'source "$HOME/.cargo/env"' >> "$HOME/.profile"
 source "$HOME/.cargo/env"
 
 # Install Python libs
