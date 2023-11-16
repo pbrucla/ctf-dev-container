6c8f9dd9d032384b86655a59d9fdb2e7f211229b
Alec <31748545+burturt@users.noreply.github.com>
2023-11-15 22:05:05 -0800

    Create installGhidra.sh

---
+++ installGhidra.sh
@@ -0,0 +1,5 @@
+# Install ghidra
+sudo apt install -y openjdk-17-jdk
+wget 'https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_10.4_build/ghidra_10.4_PUBLIC_20230928.zip'
+unzip 'ghidra_10.4_PUBLIC_20230928.zip'
+rm 'ghidra_10.4_PUBLIC_20230928.zip'
