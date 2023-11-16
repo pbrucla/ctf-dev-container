8413c7afd4eccf64ed4b80390fe4a91c910ba4df
Alec <31748545+burturt@users.noreply.github.com>
2023-11-15 22:10:40 -0800

    Update installGhidra.sh

--- installGhidra.sh
+++ installGhidra.sh
@@ -2,4 +2,7 @@
 sudo apt install -y openjdk-17-jdk
 wget 'https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_10.4_build/ghidra_10.4_PUBLIC_20230928.zip'
 unzip 'ghidra_10.4_PUBLIC_20230928.zip'
-rm 'ghidra_10.4_PUBLIC_20230928.zip'
+sudo rm 'ghidra_10.4_PUBLIC_20230928.zip'
+sudo rm -rf /opt/ghidra
+sudo mv ./ghidra_10.4_PUBLIC /opt/ghidra
+ln -sf /opt/ghidra/ghidraRun /usr/local/bin/ghidra
